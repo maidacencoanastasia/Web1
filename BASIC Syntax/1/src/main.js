79c6e278b257056ca6b5534e1a31cb58e4ee479d
maidacencoanastasia <maidacenco100@gmail.com>
2021-03-04 22:05:42 +0200

    exercise

---
+++ BASIC Syntax/1/src/main.js
@@ -0,0 +1,33 @@
+//let n = 1;
+n = Math.floor(Math.random() * 10);
+switch (n) {
+  case 1:
+    alert("один");
+    break;
+  case 2:
+    alert("два");
+    break;
+  case 3:
+    alert("три");
+    break;
+  case 4:
+    alert("четыре");
+    break;
+  case 5:
+    alert("пять");
+    break;
+  case 6:
+    alert("шесть");
+    break;
+  case 7:
+    alert("семь");
+    break;
+  case 8:
+    alert("восемь");
+    break;
+  case 9:
+    alert("девять");
+    break;
+  default:
+    alert("Нет таких значений");
+}
---
+++ BASIC Syntax/9/src/main.js
@@ -0,0 +1,8 @@
+let name = prompt("NAME,SURNAME?");
+alert(`Hello, ${name}!`);
+const result = confirm('Вам нравится JavaScript?');
+if (result) {
+alert('Молодец');
+} else {
+alert ('Можно начать изучение прямо сейчас');
+}
