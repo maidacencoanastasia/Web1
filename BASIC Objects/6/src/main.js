176f00dc4bc396b68ca6d206ab999a9099f51cdc
maidacencoanastasia <maidacenco100@gmail.com>
2021-03-04 21:53:48 +0200

    exercise

--- BASIC Objects/6/src/main.js
+++ BASIC Objects/6/src/main.js
@@ -1,32 +1,19 @@
-let arr = [{x:1, y:2}, {x:3, y:4}, {x:null, y:4}, {x:3, y:undefined}];
-let final_array =[];
+let arr = [
+  { x: 1, y: 2 },
+  { x: 3, y: 4 },
+  { x: null, y: 4 },
+  { x: 3, y: undefined },
+];
+let final_array = [];
 let i = 0;
-const output = arr.map(({ x, y }) => ([y , x]))
+const output = arr.map(({ x, y }) => [y, x]);
 console.log(output);
-for(element in output){
-    for(element1 in output[element]){
-    if(output[element1] != null || output[element1] != undefined){
-        final_array[i]=output[element1];
-        i++;
+for (element in output) {
+  for (element1 in output[element]) {
+    if (output[element1] != null || output[element1] != undefined) {
+      final_array[i] = output[element1];
+      i++;
     }
+  }
 }
-}
-console.log(final_array)
-// let result = arr.map(a => {
-//     let key = Object.keys(a)[0];
-//     return {
-//       text: key,
-//       size: a[key]
-//     }
-//   });
-  
-//   console.log(result);
-
-// const newArray = arr.map(item => {
-//     const key = Object.keys(item)[0];
-//     // Add some guard to handle empty objects..
-  
-//     return { text: key, size: item[key] };
-//   });
-// //const newArray = arr.filter(element => Number.isInteger(element))
-// console.log(newArray)
+console.log(final_array);
