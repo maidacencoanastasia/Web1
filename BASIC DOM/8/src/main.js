34ced348b675de4d465e99f7dd717e7de1efaa7b
maidacencoanastasia <maidacenco100@gmail.com>
2021-03-18 15:33:59 +0200

    Add files

---
+++ BASIC DOM/8/src/main.js
@@ -0,0 +1,32 @@
+const textInput = document.getElementById('input');
+const submit = document.getElementById('btn');
+const list = document.getElementById('list');
+
+submit.disabled = true;
+submit.addEventListener('click', () => {
+    addElementToList();
+})
+
+textInput.addEventListener('input', () => {
+    checkifEmpty();
+});
+
+let addElementToList = () => {
+    let newItem = document.createElement('li');
+    newItem.innerHTML = textInput.value;
+    newItem.className = 'list__element';
+    newItem.addEventListener('click', () => {
+        alert(newItem.innerHTML);
+    });
+    list.append(newItem);
+    textInput.value = '';
+    checkifEmpty();
+}
+
+let checkifEmpty = () => {
+    if (textInput.value !== '') {
+        submit.disabled = false;
+    } else {
+        submit.disabled = true;
+    }
+}
